d311e5728b23161e5b84492b818944c6bd197ccb
Connor Duncan <connorduncan1223@gmail.com>
2024-08-29 15:48:50 +0200

    Update password for login string in appsettings

--- Models/User.cs
+++ Models/User.cs
@@ -6,8 +6,8 @@
         public string UserName { get; set; }
         public string Email { get; set; }
         public string Address { get; set; }
-        public string Phone { get; set; }
-        public string Password { get; set; }
+        public string Phone { get; set; }                           
+        public string Password { get; set; }    
         public string Role { get; set; }
     }
 }
