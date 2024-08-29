2d835b87f22dbd6dfe09c86372404e99351d11b2
Connor Duncan <connorduncan1223@gmail.com>
2024-08-29 18:48:43 +0200

    Added the register view model.

---
+++ ViewModels/RegisterViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SAOnlineMart.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage = "Passwords do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        [Phone]
+        public string Phone { get; set; }
+    }
+}
