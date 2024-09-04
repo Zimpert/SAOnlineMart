a36b5da9c4889627257e793ab8e4838379829616
Connor Duncan <connorduncan1223@gmail.com>
2024-09-04 11:59:24 +0200

    delcare session context object as nullable.

--- Extensions/SessionExtensions.cs
+++ Extensions/SessionExtensions.cs
@@ -10,7 +10,7 @@ namespace SAOnlineMart.Extensions
             session.SetString(key, JsonConvert.SerializeObject(value));
         }
 
-        public static T GetObjectFromJson<T>(this ISession session, string key)
+        public static T? GetObjectFromJson<T>(this ISession session, string key)
         {
             var value = session.GetString(key);
             return value == null ? default(T) : JsonConvert.DeserializeObject<T>(value);
